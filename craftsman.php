2d98924fad0021a876335ac353def1af2718afd8
80characters <80chars@gmail.com>
2019-08-03 11:04:58 +0700

    add php-docs

--- craftsman.php
+++ craftsman.php
@@ -1,18 +1,29 @@
 <?php
-/*
-Plugin Name: Craftsman
-Plugin URI: http://80chars.com/plugins/reloaded
-Description: Additional energy for a wordpress theme. Which created by love and coffee, for me and for you.
-Version: 19.07.06
-Author: 80characters
-Author URI: http://80chars.com
-License: GNU General Public License v3 or later
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
-Requires at least: 5.0
-Tested up to: 5.2.2
-Text Domain: craftsman
-Domain Path: /languages/
-*/
+/**
+ * Additional energy for a WordPress theme.
+ * Which created by love and coffee, for me and for you.
+ * php version 5.6
+ *
+ * @category Root
+ * @package  Craftsman
+ * @author   80characters <80chars@gmail.com>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.html GPL-3
+ * @link     http://80chars.com
+ * @since    19.07.06
+ *
+ * Description: Additional energy for a WordPress theme. Which created by love and coffee, for me and for you.
+ * Plugin Name: Craftsman
+ * Plugin URI: http://80chars.com/plugins/reloaded
+ * Version: 19.07.06
+ * Author: 80characters
+ * Author URI: http://80chars.com
+ * License: GNU General Public License v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Requires at least: 5.0
+ * Tested up to: 5.2.2
+ * Text Domain: craftsman
+ * Domain Path: /languages/
+ */
 
 global $craftsman, $jirvas;
 
@@ -22,8 +33,8 @@ define('CRAFTSMAN_DIR_PATH', plugin_dir_path(__FILE__));
 $vendor = CRAFTSMAN_DIR_PATH . 'vendor/autoload.php';
 
 if (file_exists($vendor)) {
-	require_once($vendor);
+    include_once $vendor;
 
-	$jirvas = \Craftsman\Jirvas::getInstance();
-	$craftsman = \Craftsman\Craftsman::getInstance();
+    $jirvas = \Craftsman\Jirvas::getInstance();
+    $craftsman = \Craftsman\Craftsman::getInstance();
 }
